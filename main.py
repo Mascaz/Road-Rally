b0114f57c0401b9bb911a81d825c43f9e4a95812
Maxwell Montes Diaz <maxwell_montes@yahoo.com>
2019-02-04 06:22:42 -0800

    Code that adds a camera that rotates at a rate of 6 degrees per second

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+from math import pi, sin, cos;
+
 from direct.showbase.ShowBase import ShowBase;
+from direct.task import Task
 
 class MyApp(ShowBase):
 
@@ -12,6 +15,17 @@ class MyApp(ShowBase):
         #Apply scale and position transforms on the model
         self.scene.setScale(0.25, 0.25, 0.25);
         self.scene.setPos(-8, 42, 0);
+
+        #Add the spinCameraTask procedure to the task manager
+        self.taskMgr.add(self.spinCameraTask, "SpinCameraTask");
+
+    #Define a procedure to move the camera
+    def spinCameraTask(self, task):
+        angleDegrees = task.time * 6.0; #Rotates camera every 6 seconds
+        angleRadians = angleDegrees * (pi / 180.0); # Radians needed to calculate position
+        self.camera.setPos(20 * sin(angleRadians), -20.0 * cos(angleRadians), 3); # Sets the position in a circular motion
+        self.camera.setHpr(angleDegrees, 0, 0); # Rotates the camera at the same rate as the angle in radians but in degrees
+        return Task.cont; #Task is returned for the task manager to handle
 
 app = MyApp();
 app.run();
