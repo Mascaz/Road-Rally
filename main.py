e691663c6bb0e7d5d0389106d0259f4505784393
Maxwell Montes Diaz <maxwell_montes@yahoo.com>
2019-02-06 19:29:26 -0800

    Empty Window Upload as starting point for Road Rally

--- main.py
+++ main.py
@@ -1,63 +1,9 @@
-from math import pi, sin, cos;
-
-from direct.showbase.ShowBase import ShowBase;
-from direct.task import Task;
-from direct.actor.Actor import Actor;
-from direct.interval.IntervalGlobal import Sequence;
-from panda3d.core import Point3;
+from direct.showbase.ShowBase import ShowBase
 
 class MyApp(ShowBase):
 
     def __init__(self):
-        ShowBase.__init__(self);
+        ShowBase.__init__(self)
 
-        #Disable the camera trackball controls
-        self.disableMouse();
-
-        #Load the environment model
-        self.scene = self.loader.loadModel("models/environment");
-        #Reparent the model to render
-        self.scene.reparentTo(self.render);
-        #Apply scale and position transforms on the model
-        self.scene.setScale(0.25, 0.25, 0.25);
-        self.scene.setPos(-8, 42, 0);
-
-        #Add the spinCameraTask procedure to the task manager
-        self.taskMgr.add(self.spinCameraTask, "SpinCameraTask");
-
-        #Load and transform the panda Actor
-        self.pandaActor = Actor("models/panda-model", {"walk": "models/panda-walk4"});
-        self.pandaActor.setScale(0.005, 0.005, 0.005);
-        self.pandaActor.reparentTo(self.render);
-        #Loop its animation
-        self.pandaActor.loop("walk");
-
-        #Create the four lerp intervals needed for the panda to walk back and forth
-        pandaPosInterval1 = self.pandaActor.posInterval(13,
-                                                        Point3(0, -10, 0),
-                                                        startPos = Point3(0, 10, 0));
-        pandaPosInterval2 = self.pandaActor.posInterval(13,
-                                                        Point3(0, 10, 0),
-                                                        startPos = Point3(0, -10, 0));
-        pandaHprInterval1 = self.pandaActor.hprInterval(3,
-                                                        Point3(180, 0, 0),
-                                                        startHpr = Point3(0, 0, 0));
-        pandaHprInterval2 = self.pandaActor.hprInterval(3,
-                                                        Point3(0, 0, 0),
-                                                        startHpr = Point3(180, 0, 0));
-
-        #Create and play the sequence that coordinates the intervals
-        self.pandaPace = Sequence(pandaPosInterval1, pandaHprInterval1, pandaPosInterval2, pandaHprInterval2,
-                                  name = "pandaPace");
-        self.pandaPace.loop();
-
-    #Define a procedure to move the camera
-    def spinCameraTask(self, task):
-        angleDegrees = task.time * 6.0; #Rotates camera every 6 seconds
-        angleRadians = angleDegrees * (pi / 180.0); # Radians needed to calculate position
-        self.camera.setPos(20 * sin(angleRadians), -20.0 * cos(angleRadians), 3); # Sets the position in a circular motion
-        self.camera.setHpr(angleDegrees, 0, 0); # Rotates the camera at the same rate as the angle in radians but in degrees
-        return Task.cont; #Task is returned for the task manager to handle
-
-app = MyApp();
-app.run();
+app = MyApp()
+app.run()
