af8e6fe71701ab167c2e94ff53516e3fe5ae24d2
Maxwell Montes Diaz <maxwell_montes@yahoo.com>
2019-02-10 22:13:24 -0800

    Panda actor is loaded and placed where car will be

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
-from direct.showbase.ShowBase import ShowBase
+from direct.showbase.ShowBase import ShowBase;
+from direct.actor.Actor import Actor;
 
 class MyApp(ShowBase):
 
     def __init__(self):
-        ShowBase.__init__(self)
+        ShowBase.__init__(self);
 
-app = MyApp()
-app.run()
+        #Load and transform the panda Actor
+        self.pandaActor = Actor("models/panda-model", {"walk": "models/panda-walk4"});
+        self.pandaActor.setScale(0.005, 0.005, 0.005);
+        self.pandaActor.setPos(0, 30, -5);
+        self.pandaActor.setHpr(180, 0, 0);
+        self.pandaActor.reparentTo(self.render);
+        #Loop its animation
+        self.pandaActor.loop("walk");
+
+app = MyApp();
+app.run();
