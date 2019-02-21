5248613559328fb3f267b8392e3066372de73310
Maxwell Montes Diaz <maxwell_montes@yahoo.com>
2019-02-20 19:05:30 -0800

    Correct Car model loaded and placed correctly in viewport

--- main.py
+++ main.py
@@ -1,19 +1,35 @@
 from direct.showbase.ShowBase import ShowBase;
 from direct.actor.Actor import Actor;
+from panda3d.bullet import BulletWorld
+from panda3d.bullet import BulletRigidBodyNode
+from panda3d.bullet import BulletBoxShape
+from panda3d.bullet import BulletPlaneShape
+from panda3d.bullet import BulletCylinderShape
+from panda3d.bullet import BulletVehicle
+from panda3d.core import Vec3
 
-class MyApp(ShowBase):
+class RoadRally(ShowBase):
 
     def __init__(self):
         ShowBase.__init__(self);
 
-        #Load and transform the panda Actor
-        self.pandaActor = Actor("models/panda-model", {"walk": "models/panda-walk4"});
-        self.pandaActor.setScale(0.005, 0.005, 0.005);
-        self.pandaActor.setPos(0, 30, -5);
-        self.pandaActor.setHpr(180, 0, 0);
-        self.pandaActor.reparentTo(self.render);
-        #Loop its animation
-        self.pandaActor.loop("walk");
+        scene = BulletWorld();
+        scene.setGravity(Vec3(0, 0, -9.81));
+        base.setBackgroundColor(0.6,0.9,0.9);
 
-app = MyApp();
+        #The ground
+        self.ground = BulletPlaneShape(Vec3(0, 0, 1,), 1)
+        self.ground_node = BulletRigidBodyNode("The ground")
+        self.ground_node.addShape(self.ground)
+        self.ground_np = render.attachNewNode(self.ground_node)
+        self.ground_np.setPos(0, 0, -2)
+        scene.attachRigidBody(self.ground_node)
+
+        #Load and transform the Car Actor
+        self.car_model = loader.loadModel("Models/Car.egg");
+        self.car_model.setPos(0, 20, -3);
+        self.car_model.setHpr(180, 0, 0);
+        self.car_model.reparentTo(self.render);
+
+app = RoadRally();
 app.run();
